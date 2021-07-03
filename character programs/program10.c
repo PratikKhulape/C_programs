5f4d517672f6d99ffe40feb35f8b9c3e4cf1bf8c
PratikKhulape <86891653+PratikKhulape@users.noreply.github.com>
2021-07-04 00:32:39 +0530

    Add files via upload

---
+++ character programs/program10.c
@@ -0,0 +1,23 @@
+/*5. Accept character from user and display its ASCII value in decimal,
+octal and hexadecimal format.
+Input : A
+Output : Decimal 65
+Octal 0101
+Hexadecimal 0X41
+*/
+#include<stdio.h>
+void Display (char ch)
+{
+   if((ch>='A')&&(ch<='Z'))
+   {
+       printf("%d\t%x\t\n",ch);
+   }
+}
+int main()
+{
+char cValue = '\0'; 
+printf("Enter the character\n");
+scanf(" %c",&cValue);
+Display(cValue);
+return 0;
+}
---
+++ character programs/program6.c
@@ -0,0 +1,19 @@
+/*1. Write a program which displays ASCII table. Table contains symbol,
+Decimal, Hexadecimal and Octal representation of every member from
+0 to 255
+*/
+#include<stdio.h> 
+void DisplayASCII()
+{
+   int i=0;
+   for(i=0;i<=127;i++)
+   {
+       printf("%d\t%x\t%c\t%o\t\n",i,i,i,i);
+   }
+
+}
+int main()
+{
+DisplayASCII();
+return 0;
+}
